6f5c1a20057496501c515ca14e06b90bc1920847
jhiekkap <jari.hiekkapelto@gmail.com>
2020-10-23 00:26:09 +0300

    hooks => ts

--- src/utils/hooks/index.ts
+++ src/utils/hooks/index.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
 
-export const useInterval = (callback, delay) => {
-    const savedCallback = useRef();
+export const useInterval = (callback:() => void, delay:number) => {
+    const savedCallback: any = useRef();
 
     // Remember the latest callback.
     useEffect(() => {

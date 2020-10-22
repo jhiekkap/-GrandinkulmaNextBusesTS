9e298f72ef0b5c9b3592846564d248b3ffb4d3c6
jhiekkap <jari.hiekkapelto@gmail.com>
2020-10-23 00:03:19 +0300

    StopSearch => ts

---
+++ src/components/StopSearch.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+interface StopSearchProps {
+    setChosenStopName: (chosenStop: string) => void;
+}
+
+const StopSearch: React.FC<StopSearchProps> = ({ setChosenStopName }) => {
+
+    const [stop, setStop] = useState('')
+
+    const handleSubmit = (e:any): void => {
+        e.preventDefault()  
+        console.log('STOP', stop)
+        setChosenStopName(stop)
+    }
+
+    return <form onSubmit={handleSubmit}>
+        <label>
+            Kokeile toista pysäkkiä:
+            <input
+                type="text"
+                value={stop}
+                onChange={(e) => setStop(e.target.value)}
+            />
+        </label>
+        <button type='submit'>Lähetä</button>
+    </form>
+}
+
+export default StopSearch

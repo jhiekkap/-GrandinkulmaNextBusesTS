36c8894ffb31bd11713b82c6b9ae8bd63461cac1
jhiekkap <jari.hiekkapelto@gmail.com>
2020-10-24 20:43:30 +0300

    taulukon ylärivi piiloon jos ei tuloksia

--- src/components/TimeTable.tsx
+++ src/components/TimeTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import StopSearch from './StopSearch'; 
+import StopSearch from './StopSearch';
 import { getTime, delayToString } from '../utils';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
@@ -23,97 +23,105 @@ const TimeTable: React.FC<TimeTableProps> = ({ chosenStops, chosenStopName, setC
             <h4>{`Pysäkkihaun "${chosenStopName}"  tulo${!isMobile ? '- ja lähtö' : ''}ajat`}</h4>
             <StopSearch setChosenStopName={setChosenStopName} />
             {chosenStops.map((stop, s) => {
-                const isRealTime = Boolean(stop.vehicles.find(vehicle => vehicle.realtime));
+                const isRealTime: Boolean = Boolean(stop.vehicles.find(vehicle => vehicle.realtime));
+                const hasVehicles: Boolean = chosenStops.length > 0 && chosenStops[0].vehicles.length > 0
                 return <div className='timetable' key={s}>
-                    {chosenStops.length === 2 && (s === 0 ? <ArrowForwardIcon /> : <ArrowBackIcon />)}
-                    <div>{`${stop.name} ${stop.code}`}</div>
-                    <table >
-                        <thead>
-                            <tr>
-                                <td>
-                                    Linja
+                    {chosenStops.length === 2
+                        && (s === 0 ? <ArrowForwardIcon /> : <ArrowBackIcon />)
+                       /*  : (chosenStops.length > 2 && chosenStops[s + 1] && stop.name === chosenStops[s + 1].name)
+                            ? <ArrowForwardIcon />
+                            : (chosenStops.length > 2 && chosenStops[s - 1] && stop.name === chosenStops[s - 1].name)
+                                ? <ArrowBackIcon />
+                                : null*/}
+                    {hasVehicles ? <div>{`${stop.name} ${stop.code}`}</div> : <div>Ei tulevia lähtöjä</div>}
+                    {hasVehicles &&
+                        <table >
+                            <thead>
+                                <tr>
+                                    <td>
+                                        Linja
                                  </td>
-                                <td>
-                                    Reitti
+                                    <td>
+                                        Reitti
                                 </td>
-                                {!isMobile && <td>
-                                    Reaaliaikainen saapumistieto
-                                </td>}
-                                {!isMobile && <td>
-                                    Aikataulun mukainen tuloaika
-                                </td>}
-                                {isRealTime && !isMobile && <td>
-                                    Arvioitu tuloaika
-                                </td>}
-                                {isRealTime && !isMobile && <td>
-                                    Tuloaika myöhässä
-                                </td>}
-                                {!isMobile && <td>
-                                    Aikataulun mukainen lähtöaika
-                                </td>}
-                                {isRealTime && !isMobile && <td>
-                                    Arvioitu lähtöaika
-                                </td>}
-                                {isRealTime && !isMobile && <td>
-                                    Lähtöaika myöhässä
-                                </td>}
-                                {isMobile && <td>
-                                    Tuloaika
-                                </td>}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {stop.vehicles.map((vehicle, i) => {
-                                const { serviceDay } = vehicle
-                                const serviceDayInMs = serviceDay * 1000
-                                const scheduledArrival = getTime(new Date(serviceDayInMs + vehicle.scheduledArrival * 1000).toString())
-                                //console.log('SCHEDULED ARRIVAL', scheduledArrival)
-                                const realtimeArrival = getTime(new Date(serviceDayInMs + vehicle.realtimeArrival * 1000).toString())
-                                //console.log('REAL TIME ARRIVAL', realtimeArrival)
-                                const arrivalDelay = delayToString(vehicle.arrivalDelay)
-                                //console.log('ARRIVAL DELAY ', arrivalDelay)
-                                const scheduledDeparture = getTime(new Date(serviceDayInMs + vehicle.scheduledDeparture * 1000).toString())
-                                //console.log('SCHEDULED DEPARTURE', scheduledDeparture)
-                                const realtimeDeparture = getTime(new Date(serviceDayInMs + vehicle.realtimeDeparture * 1000).toString())
-                                //console.log('REAL TIME DEPARTURE', realtimeDeparture)
-                                const departureDelay = delayToString(vehicle.departureDelay)
-                                //console.log('DEPARTURE DELAY ', departureDelay)
-
-                                return <tr key={i}>
-                                    <td>
-                                        {vehicle.line}
-                                    </td>
-                                    <td>
-                                        {vehicle.route}
-                                    </td>
                                     {!isMobile && <td>
-                                        {vehicle.realtime ? 'KYLLÄ' : 'EI'}
-                                    </td>}
+                                        Reaaliaikainen saapumistieto
+                                </td>}
                                     {!isMobile && <td>
-                                        {scheduledArrival}
-                                    </td>}
+                                        Aikataulun mukainen tuloaika
+                                </td>}
                                     {isRealTime && !isMobile && <td>
-                                        {realtimeArrival}
-                                    </td>}
+                                        Arvioitu tuloaika
+                                </td>}
                                     {isRealTime && !isMobile && <td>
-                                        {arrivalDelay}
-                                    </td>}
+                                        Tuloaika myöhässä
+                                </td>}
                                     {!isMobile && <td>
-                                        {scheduledDeparture}
-                                    </td>}
+                                        Aikataulun mukainen lähtöaika
+                                </td>}
                                     {isRealTime && !isMobile && <td>
-                                        {realtimeDeparture}
-                                    </td>}
+                                        Arvioitu lähtöaika
+                                </td>}
                                     {isRealTime && !isMobile && <td>
-                                        {departureDelay}
-                                    </td>}
+                                        Lähtöaika myöhässä
+                                </td>}
                                     {isMobile && <td>
-                                        {vehicle.realtime ? realtimeArrival : scheduledArrival}
-                                    </td>}
+                                        Tuloaika
+                                </td>}
                                 </tr>
-                            })}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {stop.vehicles.map((vehicle, i) => {
+                                    const { serviceDay, line, route, realtime } = vehicle
+                                    const serviceDayInMs = serviceDay * 1000
+                                    const scheduledArrival = getTime(new Date(serviceDayInMs + vehicle.scheduledArrival * 1000).toString())
+                                    //console.log('SCHEDULED ARRIVAL', scheduledArrival)
+                                    const realtimeArrival = getTime(new Date(serviceDayInMs + vehicle.realtimeArrival * 1000).toString())
+                                    //console.log('REAL TIME ARRIVAL', realtimeArrival)
+                                    const arrivalDelay = delayToString(vehicle.arrivalDelay)
+                                    //console.log('ARRIVAL DELAY ', arrivalDelay)
+                                    const scheduledDeparture = getTime(new Date(serviceDayInMs + vehicle.scheduledDeparture * 1000).toString())
+                                    //console.log('SCHEDULED DEPARTURE', scheduledDeparture)
+                                    const realtimeDeparture = getTime(new Date(serviceDayInMs + vehicle.realtimeDeparture * 1000).toString())
+                                    //console.log('REAL TIME DEPARTURE', realtimeDeparture)
+                                    const departureDelay = delayToString(vehicle.departureDelay)
+                                    //console.log('DEPARTURE DELAY ', departureDelay)
+
+                                    return <tr key={i}>
+                                        <td>
+                                            {line}
+                                        </td>
+                                        <td>
+                                            {route}
+                                        </td>
+                                        {!isMobile && <td>
+                                            {realtime ? 'KYLLÄ' : 'EI'}
+                                        </td>}
+                                        {!isMobile && <td>
+                                            {scheduledArrival}
+                                        </td>}
+                                        {isRealTime && !isMobile && <td>
+                                            {realtimeArrival}
+                                        </td>}
+                                        {isRealTime && !isMobile && <td>
+                                            {arrivalDelay}
+                                        </td>}
+                                        {!isMobile && <td>
+                                            {scheduledDeparture}
+                                        </td>}
+                                        {isRealTime && !isMobile && <td>
+                                            {realtimeDeparture}
+                                        </td>}
+                                        {isRealTime && !isMobile && <td>
+                                            {departureDelay}
+                                        </td>}
+                                        {isMobile && <td>
+                                            {realtime ? realtimeArrival : scheduledArrival}
+                                        </td>}
+                                    </tr>
+                                })}
+                            </tbody>
+                        </table>}
                 </div>
             })}
         </div>
--- src/service/index.ts
+++ src/service/index.ts
@@ -1,11 +1,11 @@
 import { client, stopQuery } from './../utils/graphQL';
-import { Vehicle, Stop} from '../types';
+import { Vehicle, Stop } from '../types';
 
-  
+
 export const getVehicles: Stop[] | any = async (chosenStopName: string) => {
 
   const result = await client().query({ query: stopQuery(chosenStopName) });
-  //console.log('QUERY RESULT', result.data.stops);
+  console.log('QUERY RESULT', result.data.stops);
   return result.data.stops.map((stop: Stop) => {
     const { name, code } = stop;
     return {

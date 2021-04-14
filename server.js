44c3c65d018814c3e13d6eed84498ee82adfb6b0
Jari Hiekkapelto <jari.hiekkapelto@gmail.com>
2021-04-14 12:01:48 +0300

    fix server port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8000 || process.env.PORT;
+const port = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, 'build')));
 

e38f6277612efec7a337756a16b7f194c3ca850b
Jari Hiekkapelto <jari.hiekkapelto@gmail.com>
2021-04-14 10:47:40 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
 
   return (
     <Container className="App" maxWidth='md'>
-      <h3>PYSÄKKIHAKU with GitHub Actions</h3>
+      <h3>PYSÄKKIHAKU with GitHub ACTIONS</h3>
       <StopSearch /> 
       <h4>{`Haun "${stopName}" pysäkit ja tulo${!isMobile ? '- ja lähtö' : ''}ajat:`}</h4>
       <SearchResults />
